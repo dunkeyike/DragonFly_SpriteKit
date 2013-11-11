56f7173deb85e1c3d5357ac775c92dcfa4bac8ed
Dunkey <dunkeyike@gmail.com>
2013-11-11 15:09:36 +0900

    init project

---
+++ DragonFly/DragonFly/Class/Sprite/Enemy/KJBossEnemyCharacter.h
@@ -0,0 +1,13 @@
+//
+//  KJBossEnemyCharacter.h
+//  shooting
+//
+//  Created by Dunkey on 2013. 11. 8..
+//  Copyright (c) 2013년 Dunkey. All rights reserved.
+//
+
+#import "KJEnemyCharacter.h"
+
+@interface KJBossEnemyCharacter : KJEnemyCharacter
+
+@end
---
+++ DragonFly/DragonFly/Class/Sprite/Enemy/KJBossEnemyCharacter.m
@@ -0,0 +1,13 @@
+//
+//  KJBossEnemyCharacter.m
+//  shooting
+//
+//  Created by Dunkey on 2013. 11. 8..
+//  Copyright (c) 2013년 Dunkey. All rights reserved.
+//
+
+#import "KJBossEnemyCharacter.h"
+
+@implementation KJBossEnemyCharacter
+
+@end
